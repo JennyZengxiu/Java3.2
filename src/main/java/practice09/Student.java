1d551e486f6b2253fa105a729841413537c41047
zx <1374040664@qq.com>
2018-07-09 23:13:21 +0800

    refactor practice09

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,8 +1,6 @@
 package practice09;
 
 public class Student extends Person{
-    private int age;
-    private String name;
     Klass klass;
     public Student(int id, String name, int age, Klass klass){
         super(id, name, age);
